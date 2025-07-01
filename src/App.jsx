340a6ec0a63b2980afe3302cf3b3a7ebee5d0c82
Kathleen <kathleen.chaiffre.pro@gmail.com>
2025-07-01 11:10:04 +0200

    Jour 1 + jour 2 de l'exercice: création de MemberCards qui affichent le contenu du fichier data

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import MemberCard from "./components/MemberCard";
+import {members} from "./data/members";
+
+function App() {
+
+  return(
+    <div className="min-h-screen bg-gray-50 p-8">
+      <h1 className="text-4xl font-bold text-center mb-8">Simplon Connect</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
+        {/* enclencement d'une boucle foreach via un .map et création de props avec l'instantciation d'un objet member de type MemberCard */}
+        {members.map((member, index) => <MemberCard member={member} key={index}></MemberCard>)}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
